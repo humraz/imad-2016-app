f11ef84ed28d3cf7cba8daccc43e73f17bef147e
humraz <humrazsait@gmail.com>
2016-09-22 16:29:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article-one', function (req, res) {
+  res.send("Article omne requested is served here");
+});
+
+app.get('/article-two', function (req, res) {
+  res.send("Article two requested is served here");
+});
+app.get('/article-three', function (req, res) {
+  res.send("Article three requested is served here");
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
