5d66298d5f1db97cbdcf6ff29a97db2ae6edbf46
humraz <humrazsait@gmail.com>
2016-09-22 16:24:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express');//create webserver
+var morgan = require('morgan');//output logs of server
 var path = require('path');
 
 var app = express();
