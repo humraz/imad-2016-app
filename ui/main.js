dbaee4081ba193df678aab5cbac1cd165e069093
humraz <humrazsait@gmail.com>
2016-09-22 22:07:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 var img = document.getElementById('madi');
+var marginleft=0;
+function moveright(){
+    marginleft= marginleft +10;
+    img.style.marginLeft= marginleft+'px';
+}
 img.onClick = function()
 {
-    img.style.marginLeft= '100px';
+    var interval = setInterval(moveright,100);
 }
