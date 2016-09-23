3fdf28a1c83b1422b6d0b123d157c42941a513d3
humraz <humrazsait@gmail.com>
2016-09-23 20:52:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/article-two', function (req, res) {
 var counter=0;
 app.get('/counter', function (req, res) {
   counter=counter+1;
-  res.send(counter.tostring());
+  res.send(counter.toString());
 });
 app.get('/article-three', function (req, res) {
   res.send("Article three requested is served here");
